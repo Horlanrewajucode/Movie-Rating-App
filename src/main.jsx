c1907c15fb7118e92463c90748fda4466136767b
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-06-27 18:51:23 +0100

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./App.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
+// import "./App.css"
 
-createRoot(document.getElementById('root')).render(
+import StarRating from "./starRating";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={5}/>
+  </StrictMode>
+);
